72b2ad8a3f00e35e946130d15bacdd120f62be4b
poko-PC\poko <meguko2002@gmail.com>
2019-05-18 21:04:07 +0900

    changed kadai title name

--- kadai1.c
+++ kadai1.c
@@ -2,11 +2,12 @@
 int main()
 {
     int max,i;                        /*数列の個数maxと数列の番号iを宣言*/
-    int a[10];                         /*max個の配列aを宣言 */
-
     /*数列の個数を入力*/
     printf("Input total number :");         /*数列の個数を変数maxに代入 */
     scanf("%d", &max);
+    
+     /*max個の配列aを宣言 */
+    int a[max];
 
     /*数列の値を入力*/
     for(i=1; i<=max; i++){               /* i=1からi=maxまで合計max個の数列の値をa[i]に格納 */
