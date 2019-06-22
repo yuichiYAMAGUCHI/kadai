a9478f4387b8118e51620653ccb91df806248498
meguko2002 <meguko2002@gmail.com>
2019-06-22 09:29:45 +0900

    コメント追加テスト

--- 0526_4.c
+++ 0526_4.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+dafasdfsa
+
 int main()
 {
   int data[10] = {-3, -10, 0, 7, 2, 7, -9, 6, -9, 4};
   int i, sqrt=0, abs=0;
   for(i=0; i<10; i++){
-    if(data[i] * data[i] > sqrt){         //trur:15‚Ì”{”
+    if(data[i] * data[i] > sqrt){         //trur:15ï¿½Ì”{ï¿½ï¿½
       sqrt = data[i] * data[i];
       abs = data[i];
     }
