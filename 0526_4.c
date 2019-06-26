eac64636045b9729bd4e1a77bdcbdd56ca1f0dcf
poko-PC\poko <meguko2002@gmail.com>
2019-06-26 22:22:01 +0900

    今までのファイルを追加

---
+++ 0526_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+  int data[10] = {-3, -10, 0, 7, 2, 7, -9, 6, -9, 4};
+  int i, sqrt=0, abs=0;
+  for(i=0; i<10; i++){
+    if(data[i] * data[i] > sqrt){         //trur:15�̔{��
+      sqrt = data[i] * data[i];
+      abs = data[i];
+    }
+  }
+  printf("Maximum absolute value is %d\n",abs);
+}
---
+++ kadai1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int max,i;                        /*数列の個数maxと数列の番号iを宣言*/
+    /*数列の個数を入力*/
+    printf("Input total number :");         /*数列の個数を変数maxに代入 */
+    scanf("%d", &max);
+    
+     /*max個の配列aを宣言 */
+    int a[max];
+
+    /*数列の値を入力*/
+    for(i=1; i<=max; i++){               /* i=1からi=maxまで合計max個の数列の値をa[i]に格納 */
+        printf("Input %dth number :", i);
+        scanf("%d", &a[i]);
+    }
+
+    /*逆順を出力*/
+    printf("Reversed order :");
+    for(i=max; i>0; i--){              /* i=maxからi=0まで合計max個の数列の値を出力*/
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
