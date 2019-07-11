d444325d83c74fa780be10e71e883b01ab2c8d30
yuichiYAMAGUCHI <yyyusan007@gmail.com>
2019-07-11 22:19:23 +0900

    0711課題

---
+++ 2Q課題/5-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+	int s=0,i,data[100],n=0,j;
+	printf("Œ…”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B");
+	scanf("%d",&n);
+	printf("1‚Ü‚½‚Í0‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B");
+	for(i=0;i<n;i++){
+		scanf("%d",&data[i]);
+	}
+	for(j=0;j<n;j++){
+		s=2*s+data[j];
+		//printf("%d\n",s);
+	}
+	printf("%2d",s);
+}
---
+++ 2Q課題/rei.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+	int s=0,i,data[100],n=0,j;
+	printf("Œ…”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B");
+	scanf("%d",&n);
+	printf("1‚Ü‚½‚Í0‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B");
+	for(i=0;i<n;i++){
+		scanf("%d",&data[i]);
+	}
+		for(i=0;i<n;i++){
+		printf("%d",data[i]);
+	}
+}
